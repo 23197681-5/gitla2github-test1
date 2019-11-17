1e6db1940ad63555e834862a5ecae3ffe0be7468
Luna <git@l4.pm>
2019-11-17 17:26:47 -0300

    add db to job_queue example

--- examples/job_queue.py
+++ examples/job_queue.py
@@ -1,5 +1,8 @@
+import os
 import logging
 import asyncio
+
+import asyncpg
 from violet import JobManager
 
 
@@ -11,7 +14,18 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     loop = asyncio.get_event_loop()
-    sched = JobManager(loop=loop)
+    sched = JobManager(
+        loop=loop,
+        db=loop.run_until_complete(
+            asyncpg.create_pool(
+                host="localhost",
+                port="5432",
+                user=os.getenv("PSQL_USER"),
+                password=os.getenv("PSQL_PASS"),
+                database=os.getenv("PSQL_DB"),
+            )
+        ),
+    )
     sched.create_job_queue(
         "my_function", args=(int, int), handler=my_function, takes=1, period=1
     )
