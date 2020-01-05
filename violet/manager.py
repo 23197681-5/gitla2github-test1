50fe3fff40a899229065d873a1542def78aaf853
Luna <git@l4.pm>
2020-01-05 15:00:56 -0300

    update copyright years
     - setup.py: mark package as typed

--- violet/manager.py
+++ violet/manager.py
@@ -1,5 +1,5 @@
 # violet: An asyncio background job library
-# Copyright 2019, elixi.re Team and the violet contributors
+# Copyright 2019-2020, elixi.re Team and the violet contributors
 # SPDX-License-Identifier: LGPL-3.0
 
 import datetime
