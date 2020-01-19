bf92a775c97aa8fabe9d4194803a01f6df84310e
Luna <git@l4.pm>
2020-01-19 14:45:50 -0300

    schema.sql: add primary_job_id, secondary_job_id

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 create table if not exists violet_jobs (
-    job_id text primary key,
+    primary_job_id text primary key,
+    secondary_job_id text UNIQUE,
     queue text default null,
     state bigint default 0,
     fail_mode text default 'log_error',
