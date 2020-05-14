2bb774a604486589ed8bb6bc5455c2515ab585d4
Luna <git@l4.pm>
2020-05-14 16:10:20 -0300

    add default job queue parameters

--- violet/queue.py
+++ violet/queue.py
@@ -1,15 +1,25 @@
-from typing import TypeVar, Generic
+from typing import Optional, Tuple
+from violet.fail_modes import FailMode
 
-QueueArgType = TypeVar("QueueArgType")
 
-
-class JobQueue(Generic[QueueArgType]):
+class JobQueue:
     """Represents a job queue."""
 
+    workers = 1
+    start_existing_jobs = True
+    fail_mode: Optional[FailMode] = None
+    poller_takes: int = 1
+    poller_seconds: float = 1.0
+
+    @property
     @classmethod
-    async def handle(_, ctx) -> None:
-        ...
+    def poller_rate(cls) -> Tuple[int, float]:
+        return cls.poller_takes, cls.poller_seconds
 
     @classmethod
     async def setup(_, ctx) -> None:
+        ...
+
+    @classmethod
+    async def handle(_, ctx) -> None:
         ...
